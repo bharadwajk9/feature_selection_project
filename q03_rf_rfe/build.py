0a27d523bedf740c7d9cb5d42a273dce7f27b787
bharadwajk9 <utkb.k9@gmail.com>
2018-06-27 14:59:44 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,15 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df):
+    model = RandomForestClassifier()
+    X,y = df.iloc[:,:-1], df.iloc[:,-1]
+    rfe = RFE(model, X.shape[1]/2)
+    rfe = rfe.fit(X,y)
+    top_features = list(X.columns.values[rfe.support_])
+    
+    return top_features
+
+rf_rfe(data)
+
 
